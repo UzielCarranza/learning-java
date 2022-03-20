8fddb1a1c875d4d566e5edfff67ed31105e1532c
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-03-20 10:34:46 -0400

    added variables to practice exercise

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello there");
-        System.out.println("pushed to github");
+        int studentAge = 15;
+        double studentGpa = 3.14;
+        char firstName = 'M';
+        char lastName= 'K';
+        boolean hasPerfectAttendance = true;
+        System.out.println(studentAge);
+        System.out.println(studentGpa);
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(hasPerfectAttendance);
+
     }
 }
