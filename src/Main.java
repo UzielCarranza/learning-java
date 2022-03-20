b8ef9b1cb1cb3173a312af97daf037b619ddaa1b
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-03-20 11:19:47 -0400

    started fortune teller

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int studentAge = 15;
-        double studentGpa = 3.14;
-        char firstName = 'M';
-        char lastName= 'K';
-        boolean hasPerfectAttendance = true;
+//        int studentAge = 15;
+//        double studentGpa = 3.14;
+//        char firstName = 'M';
+//        char lastName= 'K';
+//        boolean hasPerfectAttendance = true;
+//
+//        String studentName = "Some Name ";
+//
+//        char studentFirstNameLetter = studentName.charAt(0);
+//
+//        char studentLastNameLetter = studentName.charAt(5);
+//        System.out.println(studentFirstNameLetter);
+//        System.out.println(studentLastNameLetter);
+//
+//        System.out.println(studentName + " has a gpa of:" + studentGpa);
+//
+//
+//        System.out.println("please update your gpa here");
+//        //get input from a user
+//        Scanner input = new Scanner(System.in);
+//        studentGpa = input.nextDouble();
+//
+//        System.out.println(studentName + " has now a gpa of:" + studentGpa);
 
-        String studentName = "Some Name ";
+        //second example fortune teller
+        System.out.println("Pick a number between 1 and 10 ");
 
-        char studentFirstNameLetter = studentName.charAt(0);
-
-        char studentLastNameLetter = studentName.charAt(5);
-        System.out.println(studentFirstNameLetter);
-        System.out.println(studentLastNameLetter);
-
-        System.out.println(studentName + " has a gpa of:" + studentGpa);
     }
 }
