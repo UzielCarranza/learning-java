9bd61abe1a3b5d521eb346c86bb3a836da5129cd
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-03-20 11:24:27 -0400

    practice with conditionals

--- src/Main.java
+++ src/Main.java
@@ -28,6 +28,13 @@ public class Main {
 
         //second example fortune teller
         System.out.println("Pick a number between 1 and 10 ");
+        Scanner scanner = new Scanner(System.in);
+        int inputtedNumber = scanner.nextInt();
+        if (inputtedNumber < 5 ){
+            System.out.println("Enjoy the good luck a friend brings you");
+        } else {
+            System.out.println("your show selection will make you very happy");
+        }
 
     }
 }
