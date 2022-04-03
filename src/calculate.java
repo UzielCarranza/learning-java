7a84b430971281b6a308c40d376bc09196493204
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-03 12:50:29 -0400

    completed function exercise

--- src/calculate.java
+++ src/calculate.java
@@ -5,5 +5,9 @@ public class calculate {
         double tip = tipRate * listedMealPrice;
         double tax = taxRate * listedMealPrice;
         double result = listedMealPrice + tip + tax;
+        System.out.println("your total meal price is: " + result);
+    }
+    public static void main (String [] args){
+        calculateTotalMealPrice(15, .2, .08);
     }
 }
