c5bc8d19881a375d5ac8bf080026af46adc171e0
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-03 12:36:16 -0400

    completed function defition example

--- src/function.java
+++ src/function.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class function {
     public static void announceDeveloperTeaTime(){
         System.out.println("waiting for developer tea time");
-        System.out.println(("enter something random to start developer tea time"));
+        System.out.println("enter something random to start developer tea time");
+
+        Scanner input = new Scanner(System.in);
+        input.next();
+        System.out.println("its developer tea time");
     }
     public static void main(String[] args){
 
