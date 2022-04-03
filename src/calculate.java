263cfc9da67b9ba09bd93a456a787190b2f253c6
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-03 13:05:55 -0400

    function returns a number

--- src/calculate.java
+++ src/calculate.java
@@ -1,11 +1,11 @@
 public class calculate {
-    public static void calculateTotalMealPrice(double listedMealPrice,
+    public static double calculateTotalMealPrice(double listedMealPrice,
                                                double taxRate,
                                                double tipRate){
         double tip = tipRate * listedMealPrice;
         double tax = taxRate * listedMealPrice;
         double result = listedMealPrice + tip + tax;
-        System.out.println("your total meal price is: " + result);
+        return result;
     }
     public static void main (String [] args){
         calculateTotalMealPrice(15, .2, .08);
