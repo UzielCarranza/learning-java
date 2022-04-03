bf9cf56155761ae05e68c0be02964417b111b3ab
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-03 12:30:12 -0400

    settled function declarations

--- src/function.java
+++ src/function.java
@@ -1,6 +1,7 @@
 public class function {
     public static void announceDeveloperTeaTime(){
-
+        System.out.println("waiting for developer tea time");
+        System.out.println(("enter something random to start developer tea time"));
     }
     public static void main(String[] args){
 
