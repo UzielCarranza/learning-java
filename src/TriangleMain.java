9449d72cc1da9605a85cbeaf5949a53dbb2eed5e
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-07 09:00:53 -0400

    finding the area of triangle A

--- src/TriangleMain.java
+++ src/TriangleMain.java
@@ -4,5 +4,8 @@ public class TriangleMain {
     public static void main(String[] args) {
         Triangle triangleA = new Triangle(15, 8, 15, 8, 17);
         Triangle triangleB = new Triangle(3, 2.598, 3, 3, 3 );
+
+        double triangleAArea = triangleA.findArea();
+        System.out.printls(triangleAArea);
     }
 }
